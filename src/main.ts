31465374dc9b96510585c365c5d3dd1a7b6149fc
王兴家 <xingwangzhe@outlook.com>
2025-03-14 18:17:22 +0800

    init,use bulma.css

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import './assets/main.css'
-
+// import './assets/main.css'
+import '../node_modules/bulma/css/bulma.min.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
